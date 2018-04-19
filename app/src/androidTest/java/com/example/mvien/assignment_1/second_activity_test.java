d42ee2fa1b89ca74ad3198ed908b859aba6b6a8e
mviencek <32376631+mviencek@users.noreply.github.com>
2018-04-19 11:41:08 -0700

    adding testing for backbutton

--- app/src/androidTest/java/com/example/mvien/assignment_1/second_activity_test.java
+++ app/src/androidTest/java/com/example/mvien/assignment_1/second_activity_test.java
@@ -1,6 +1,7 @@
 package com.example.mvien.assignment_1;
 
 import android.content.Intent;
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -35,6 +36,19 @@ public class second_activity_test {
     //tests the go back button and makes sure the form is empty
     public void goBack(){
         onView(withId(R.id.mainActivityButton)).perform(click());
+        onView(withId(R.id.ageEditText)).check(matches(withText("")));
+        onView(withId(R.id.usernameEditText)).check(matches(withText("")));
+        onView(withId(R.id.emailEditText)).check(matches(withText("")));
+        onView(withId(R.id.ageText)).check(matches(withText("")));
+        onView(withId(R.id.nameEditText)).check(matches(withText("")));
+    }
+
+    @Test
+    //tests the go back button and makes sure the form is empty
+    public void goBackButton() {
+        onView(withId(R.id.textView))
+                .check(matches(withText("Thanks for signing up Mikal Viencek!\n")));
+        Espresso.pressBack();
         onView(withId(R.id.ageEditText)).check(matches(withText("")));
         onView(withId(R.id.usernameEditText)).check(matches(withText("")));
         onView(withId(R.id.emailEditText)).check(matches(withText("")));
