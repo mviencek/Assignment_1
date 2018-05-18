bc7fadf9693788975b1ced77841ede8912566bce
mviencek <32376631+mviencek@users.noreply.github.com>
2018-05-18 12:51:07 -0700

    cleaning up imports

--- app/src/main/java/com/example/mvien/assignment_1/MainActivity.java
+++ app/src/main/java/com/example/mvien/assignment_1/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.mvien.assignment_1;
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
@@ -7,25 +6,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import android.app.DatePickerDialog;
 import android.app.Dialog;
-
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Calendar;
 import android.widget.DatePicker;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private Button secondActivityBtn;
