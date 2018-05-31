557452856e90b4a3ee82839554de93fd35c3f419
mviencek <32376631+mviencek@users.noreply.github.com>
2018-05-31 11:52:06 -0700

    adding rule to main test file

--- app/src/androidTest/java/com/example/mvien/assignment_1/main_activity_test.java
+++ app/src/androidTest/java/com/example/mvien/assignment_1/main_activity_test.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -34,6 +35,8 @@ public class main_activity_test {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule
             = new ActivityTestRule<>(MainActivity.class);
+
+    @Rule public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
 
 
     @Test
